0d1f4d4a4931245528051dd8dabfd9340ee7c738
MuriloM <muriloommaia@gmail.com>
2022-02-10 15:50:06 -0300

    corrige eslint

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => res.send(`<h1> Executando na porta ${port} </h1>`));
 app.get('/alow', (req, res) => res.send(`<h1> Executando na porta ${port} </h1>`));
 
